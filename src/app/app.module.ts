7a3a3ad08043a97a78230f68e52fd646bc160051
Wesley Nascimento <ylnask@gmail.com>
2023-04-12 22:55:58 -0300

    Angular projeto resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewPostComponent,
+    TweetComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
